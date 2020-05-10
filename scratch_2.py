bc03ba5eee9f63f1b425ed479de80494eaffff89
SumedhSarode <s.sumedh@gmail.com>
2020-05-10 23:44:29 +0530

    snacks code added

---
+++ scratch_2.py
@@ -0,0 +1,9 @@
+hungry=input("Enter Yes or No for hungry")
+if hungry=="yes":
+   print("I am Hungry")
+if hungry =="pizza":
+   print("Unhealthy snack")
+if hungry == "Burfer":
+      print("Very Unhealthy snack")
+else:
+   print("Do homework")
