539cd29079d7018ab64d58b95c0339989f03d497
SumedhSarode <s.sumedh@gmail.com>
2020-05-10 21:41:07 +0530

    first version of code

---
+++ basicpractice1.py
@@ -0,0 +1,11 @@
+
+
+num = int (input(Enter  the  number))
+n=num
+for i in range(1,300):
+    d1=n%10
+    n=n//10
+    d2=n%10
+    n=n//10
+    if d1*d1*d1 +d2*d2*d2+n*n*n==num:
+        print(num)
