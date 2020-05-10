3802bcd0600b665eb4f74e5ac0443e30cd717137
SumedhSarode <s.sumedh@gmail.com>
2020-05-11 00:06:31 +0530

    added Drink with snacks

--- scratch_2.py
+++ scratch_2.py
@@ -1,9 +1,15 @@
 hungry=input("Enter Yes or No for hungry")
+thirsty=input("Enter drink")
 if hungry=="yes":
    print("I am Hungry")
 if hungry =="pizza":
    print("Unhealthy snack")
 if hungry == "Burfer":
       print("Very Unhealthy snack")
+if thirsty=="Coke":
+    print("Coke is good with bburger")
+if thirsty=="Milk":
+    print("Milk is good when hungry")
+if 
 else:
    print("Do homework")
