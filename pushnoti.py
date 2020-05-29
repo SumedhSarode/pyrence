2fc872513aee74d3b0107bb2ce48aafda2217262
SumedhSarode <s.sumedh@gmail.com>
2020-05-29 18:03:45 +0530

    first egg python code

---
+++ pushnoti.py
@@ -0,0 +1,11 @@
+applecost = 10
+no_of_apples_per_dozen = 12
+applewholesale = 10
+applepurchased = int(input("Enter apples"))
+if applepurchased>100:
+    totalsale = no_of_apples_per_dozen*applepurchased*applecost
+else:
+    totalsale = applewholesale * applepurchased * applecost
+
+print(totalsale)
+
