0ba6a47a3d8e4b876d0826e9d3b2befc4b6fbeab
SumedhSarode <s.sumedh@gmail.com>
2020-05-29 18:42:11 +0530

    added oranges for 3rd time

--- pushnoti.py
+++ pushnoti.py
@@ -3,6 +3,7 @@ no_of_apples_per_dozen = 12
 applewholesale = 10
 applepurchased = int(input("Enter apples"))
 insert2mango=20
+insert3orange=50
 if applepurchased>100:
     totalsale = no_of_apples_per_dozen*applepurchased*applecost
 else:
@@ -10,4 +11,5 @@ else:
 
 print(totalsale)
 print(insert2mango)
+print(insert3orange)
 
