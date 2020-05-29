adc6a88a2296ce55ba9f131bfc428c44f13822d5
SumedhSarode <s.sumedh@gmail.com>
2020-05-29 18:25:28 +0530

    mangoman

--- pushnoti.py
+++ pushnoti.py
@@ -2,10 +2,12 @@ applecost = 10
 no_of_apples_per_dozen = 12
 applewholesale = 10
 applepurchased = int(input("Enter apples"))
+insert2mango=20
 if applepurchased>100:
     totalsale = no_of_apples_per_dozen*applepurchased*applecost
 else:
     totalsale = applewholesale * applepurchased * applecost
 
 print(totalsale)
+print(insert2mango)
 
